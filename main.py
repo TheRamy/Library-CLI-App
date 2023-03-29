a5236e1b06fb35439a522d2da8bdaa5085578b95
TheRamy <122477951+TheRamy@users.noreply.github.com>
2023-03-29 20:11:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,6 @@ def start():
 def sign_up(username: str):
     console.clear()
     typer.echo(f"Nice that you are signing up!")
-    # TODO: Add user with name {username} to database table
 
     util.cli.cli_add_user(username)
 
@@ -41,6 +40,36 @@ def sign_in(username: str, password: str):
     console.clear()
 
     util.cli.cli_login(username, password)
+
+
+@app.command("search_by_name")
+def search_by_name(name: str):
+
+    console.clear()
+
+    table = Table(show_header=True, header_style="bold white")
+    # table.add_column("Column 1", style="dim", width=10)
+    table.add_column("#", style="dim", min_width=10, justify=True)
+    table.add_column("Book ID", style="dim", min_width=10, justify=True)
+    table.add_column("Name", style="dim", min_width=10, justify=True)
+    table.add_column("Author", style="dim", min_width=10, justify=True)
+    table.add_column("# Pages", style="dim", min_width=10, justify=True)
+    table.add_column("Genre", style="dim", min_width=10, justify=True)
+    table.add_column("Availability", style="dim", min_width=10, justify=True)
+
+    # util.db.example_table()
+    table_db = util.db.sql_select(
+        f"SELECT * from books WHERE lower(book_name) = '{name}'", "fetchall")
+
+    i = 1
+    for row in table_db:
+        table.add_row(str(i), str(row[0]), str(row[1]), str(
+            row[2]), str(row[3]), str(row[4]), str(row[5]))
+        i += 1
+
+    typer.secho(
+        f'You searched for a book named "{name}" so here is the result:', fg='white')
+    console.print(table)
 
 
 @app.command("display_table")
